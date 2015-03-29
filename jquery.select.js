16a920f080a77f63e6173aac3e57fb1222aedd2c
fadonascimento <fadonascimento@ig.com.br>
2015-03-29 16:26:18 -0300

    Update

--- jquery.select.js
+++ jquery.select.js
@@ -1,16 +1,56 @@
-( function( $ ){
-    $.fn.selectBox = function(){
-        return this.each(function(){ 
-            $(this).css({'position':'absolute'});
-            if( $('option:selected', this).val() !== ''  ) 
-                title = $('option:selected',this).text();
-            $(this).css({'z-index':10,'opacity':0,'-khtml-appearance':'none'})
-                   .after('<span class="select" style="position:absolute; width:' + $(this).css('width') +'; padding: 0 0 0 30px;">' + title + '</span>')
-                   .change(function(){
-                        val = $('option:selected',this).text();
-                        $(this).next().text(val);
-                    });
++function ($) {
 
+    'use strict';
+    /**
+    * noSelect plugin, very slightly modified
+    * http://mths.be/noselect v1.0.3
+    *
+    * @param jQuery $elem Element that we don't want to select
+    * @param Object options Uniform options for the element
+    */
+    function noSelect ($element) {
+
+        var none = 'none';
+        $element.bind('selectstart dragstart mousedown', function() {
+            return false;
+        }).css({
+            'MozUserSelect': none,
+            'msUserSelect': none,
+            'webkitUserSelect': none,
+            'userSelect': none
         });
     }
-})(jQuery);
+
+    $.fn.selectBox = function(){
+        return this.each(function(){
+
+            var $this = $(this);
+            var title = $this.find(":selected").html();
+            var $divWrapper = $("<div class='selector'></div>");
+            var $span = $('<span></span>');
+
+            $span.html($this.find(":selected").html());
+            $this.wrap($divWrapper)
+            .before($span)
+            .change(function(){
+                $span.html($this.find(":selected").html());
+            })
+            .on('click touchend', function() {
+                // IE7 and IE8 may not update the value right
+                // until after click event - issue #238
+                var selHtml = $this.find(":selected").html();
+
+                if ($span.html() !== selHtml) {
+                // Change was detected
+                // Fire the change event on the select tag
+                $this.trigger('change');
+            }
+            }).on('keyup', function() {
+                $span.html($this.find(":selected").html());
+            });
+            noSelect($span);
+        });
+    };
+}(jQuery);
+
+
